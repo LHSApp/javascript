7dcde963533a9dd9c55fe9dfc3579a803bd144e9
Leandro <leandrosantos020788@gmail.com>
2020-08-17 12:27:40 -0300

    Condicional aula 5

--- Primeros passos no javascript/index.js
+++ Primeros passos no javascript/index.js
@@ -10,14 +10,26 @@
 
 
 const aluno1 = 'Leandro'  // varialvel string
+const notaaluno1 = 1.8 // variavel number
+
 const aluno2 = 'Diego'
-const alino = 'Lilian'
-const notaaluno1 = 9.8 // variavel number
 const notaaluno2 = 10
+
+const aluno3 = 'Lilian'
 const notaaluno3 = 2
+
 
 const media = (notaaluno1 + notaaluno2 + notaaluno3) / 3
 
-console.log(media)
+// se a média for maior que 5, parabenizar a turma
+//Estrutra condicional
+if (media > 5) {
+  //faz alguma coisa 
+  console.log(`A media  foi de ${media}. Parabéns`)
+} else {
+  //faz outra coisa 
+  console.log('A média é menor  que 5')
+}
+
 
 
