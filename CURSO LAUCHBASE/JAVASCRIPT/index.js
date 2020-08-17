dda647cc286b06367471714bcbc96fdc9157b319
Leandro <leandrosantos020788@gmail.com>
2020-08-17 11:39:08 -0300

    Primeiros passos para o JAVASCRIPT

---
+++ CURSO LAUCHBASE/JAVASCRIPT/index.js
@@ -0,0 +1,23 @@
+//const nome = 'Leandro JAVASCRIPT'
+
+//console.log(nome)
+
+
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do calculo da média
+
+
+
+const aluno1 = 'Leandro'  // varialvel string
+const aluno2 = 'Diego'
+const alino = 'Lilian'
+const notaaluno1 = 9.8 // variavel number
+const notaaluno2 = 10
+const notaaluno3 = 2
+
+const media = (notaaluno1 + notaaluno2 + notaaluno3) / 3
+
+console.log(media)
+
+
