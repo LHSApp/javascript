0532b82e76f62e90aa883cc6a0f58aee08ebc5f7
Leandro <leandrosantos020788@gmail.com>
2020-08-18 12:50:48 -0300

    Calculo do imc

---
+++ Primeros passos no javascript/calculo_de_imc.js
@@ -0,0 +1,16 @@
+const nome = 'Leandro'
+const peso = 84
+const altura = 1.88
+const sexo = 'masculino'
+const imc =  peso/(altura * altura);
+
+if ( imc >= 30){
+
+  console.log( 'você está acima do peso')
+}else if(imc <= 29.9){
+  console.log( 'você não está acima do peso')
+}
+
+
+
+
