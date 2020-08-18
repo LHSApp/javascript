cd486ea61b789913d2c70d6f240240f9f732efeb
Leandro <leandrosantos020788@gmail.com>
2020-08-17 22:56:57 -0300

    Operadores

---
+++ Primeros passos no javascript/operadores.js
@@ -0,0 +1,38 @@
+//console.log( 5 > 4) //true
+
+//console.log( 5 < 4) //false
+
+//console.log( 5 >= 4)//true
+
+//console.log( 4 <= 4)//true
+
+
+//console.log( 4 == "4") //true
+
+//console.log( 4 === "4") //false
+
+//console.log( 4 != "5")//true
+
+//console.log( 4 !== "5")//true
+
+//DESAFIO 1
+
+const idade = 17
+
+ //verificar se a pessoa é maior igual a  18 anos
+
+if(idade >= 18){
+
+  console.log('deixar entrar')
+
+} else {
+  console.log('Bloquear a entrada')
+}
+ // se sim , deixar entrar. se não , bloquear a entrada
+
+// se a pessoativer 17 anos
+if(idade === 17){
+
+  console.log('volte quando tiver 18')
+}
+
