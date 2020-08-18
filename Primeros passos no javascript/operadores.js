73c07db8bcb21625649ee9a8325d244ab445e3cf
Leandro <leandrosantos020788@gmail.com>
2020-08-18 11:16:12 -0300

    Operadores
    OPERADORES DE COMPARAÇÃO
    OPERADORES DE LÓGICOS
    OPERADORES DE ARITMÉTICOS

--- Primeros passos no javascript/operadores.js
+++ Primeros passos no javascript/operadores.js
@@ -1,3 +1,15 @@
+/*----------------------------------------------------------------
+  OPERADORES DE COMPARAÇÃO
+
+> MAIOR
+< MENOR
+>= MAIOR IGUAL A
+<= MENOR IGUAL A
+== IGUAL A
+=== IGUAL E DO MESMO TIPO
+!= DIFERENTE DE
+!===  DIFERENTE, INCLUSIVE DO TIPO
+   
 //console.log( 5 > 4) //true
 
 //console.log( 5 < 4) //false
@@ -15,24 +27,62 @@
 
 //console.log( 4 !== "5")//true
 
+//----------------------------------------------------------------*/
+
+/*----------------------------------------------------------------//
+//const idade = 16  // Bloquear a entrada// ou //18 deixar entrar//
 //DESAFIO 1
+//verificar se a pessoa é maior igual a  18 anos
+// se sim , deixar entrar. se não , bloquear a entrada
+// se a pessoa tiver 17 anos
+ 
+//if(!(idade >= 18) || idade === 17 ){
+  //console.log('Bloquear a entrada')
+ 
 
-const idade = 17
+//} else {
+ // console.log('deixar entrar')
+//}
 
- //verificar se a pessoa é maior igual a  18 anos
 
-if(idade >= 18){
+//----------------------------------------------------------------*/
 
-  console.log('deixar entrar')
+/*----------------------------------------------------------------
 
-} else {
-  console.log('Bloquear a entrada')
-}
- // se sim , deixar entrar. se não , bloquear a entrada
+// OPERADORES DE LÓGICOS
 
-// se a pessoativer 17 anos
-if(idade === 17){
+//-------------------------------------------------
+// && "E" as duas condiçores devem ser verdadeiras
+para que a condição final seja verdadeira.
+//console.log(5 == 5 && 6 == 6)// true
+//console.log(5 == 5 && 6 != 6)// false
 
-  console.log('volte quando tiver 18')
-}
+//-------------------------------------------------
+// || "OU" Uma das condições deve ser verdadeira
+para que a condição final seja veradeira.
+//console.log(5 != 5 || 6 == 6)// true
+//console.log(5 == 5 || 6 != 6)// true
 
+//-------------------------------------------------
+// "Não" Nega um condição
+// NEGANDO O FALSO
+//console.log( !( 5 > 6)) // true
+//console.log( !( 5 < 6)) // false
+
+//----------------------------------------------------------------*/
+
+/*-----------------------------------------------
+ OPERADORES DE ARITMÉTICOS
+
+  * MULTIPLICAÇÃO
+  / DIVISÃO
+  % RESTO DA DIVISÃO
+  + ADIÇÃO
+  -SUBTRAÇÃO
+
+console.log( 2 * 2 ) // 4
+console.log( 2 / 2) // 1
+console.log(2 % 1.5) // 0.5
+console.log( 2 + 2) // 4
+console.log(2 - 2 ) // 0
+---------------------------------------------*/
