086f68467492b44fca48fdfe8fc87624816c4034
Leandro <leandrosantos020788@gmail.com>
2020-08-26 12:29:23 -0300

    Vetores e objetos

--- Primeros passos no javascript/vetores_e_objetos.js
+++ Primeros passos no javascript/vetores_e_objetos.js
@@ -1,16 +1,17 @@
-
 // Vetores e objetos
 
 const objeto = {
   propriedade: [
     { nome: "C++", especialidade: "Desktop" },
-    { nome: "JavaScript", especialidade: "Web/Mobile" }
-  ]
+    { nome: "JavaScript", especialidade: "Web/Mobile" },
+  ],
 };
 
 const usuario = {
   nome: "LHSApp",
-  idade: 32
-}
-  
-console.log(`O usuário ${usuario.nome} tem  ${usuario.idade} anos e usa a tecnologia ${objeto.propriedade[0].nome} com especialidade em ${objeto.propriedade[0].especialidade}! `)
+  idade: 32,
+};
+
+console.log(
+  `O usuário ${usuario.nome} tem  ${usuario.idade} anos e usa a tecnologia ${objeto.propriedade[0].nome} com especialidade em ${objeto.propriedade[0].especialidade}! `);
+
