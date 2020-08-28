bf10ce80b532d37f0fce77d9ed38d8a27fa2f0f2
Leandro <leandrosantos020788@gmail.com>
2020-08-28 10:37:21 -0300

    Calculo méida Aluno

--- Primeros passos no javascript/calculo_media_aluno.js
+++ Primeros passos no javascript/calculo_media_aluno.js
@@ -2,10 +2,7 @@
 // das notas entre os alunos e envia 
 // mensagem do cálculo da média.
 
-
-
-
-const alunos = [
+const alunosDaTumaA = [
   {
     nome: "Leandro",
     nota: 9.8
@@ -18,12 +15,51 @@ const alunos = [
 
   {
     nome: "Felipe",
-    nota: 3
+    nota: 2
   
   }
 ]
 
+const alunosDaTumaB = [
+  {
+    nome: "Leandro1",
+    nota: 10
+  },
+  {
+    nome: "Lilian2",
+    nota: 5
 
-const nomesDeAlunos = ["Leandro", "Lilian", "Felipe"]
+  },
 
-console.log(alunos)
+  {
+    nome: "Felipe3",
+    nota: 0
+  
+  }
+]
+
+function  calculaMedia(alunos) {
+
+  return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+}
+
+
+const media1 = calculaMedia(alunosDaTumaA)
+const media2 = calculaMedia(alunosDaTumaB)
+
+
+
+function enviaMensagem(media, turma) {
+
+  // Se a média for maior que 5, parabenizar a turma
+
+  if (media > 5) {
+
+    console.log( `A média da turma ${turma} foi de  ${media}. Parabéns`)
+  } else {
+    console.log(`A média da turma ${turma} menosr que 5`)
+  }
+}
+
+enviaMensagem(media1, 'Turma A')
+enviaMensagem(media2, 'Turma B')
