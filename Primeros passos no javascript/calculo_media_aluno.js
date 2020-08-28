7befa823421b3ab1c12de92633f9467f088fc9c0
Leandro <leandrosantos020788@gmail.com>
2020-08-28 09:59:12 -0300

    Calculo de média do Aluno

---
+++ Primeros passos no javascript/calculo_media_aluno.js
@@ -0,0 +1,29 @@
+// Criar um programa para calcular a media 
+// das notas entre os alunos e envia 
+// mensagem do cálculo da média.
+
+
+
+
+const alunos = [
+  {
+    nome: "Leandro",
+    nota: 9.8
+  },
+  {
+    nome: "Lilian",
+    nota: 10
+
+  },
+
+  {
+    nome: "Felipe",
+    nota: 3
+  
+  }
+]
+
+
+const nomesDeAlunos = ["Leandro", "Lilian", "Felipe"]
+
+console.log(alunos)
